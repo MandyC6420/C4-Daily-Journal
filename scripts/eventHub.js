e1d83e96aaa069c7fb001ecf8ca4c1be064084b9
Mandy <drgnfli6420@yahoo.com>
2021-02-01 15:23:16 -0500

    entries are saving

--- scripts/eventHub.js
+++ scripts/eventHub.js
@@ -1,17 +1,17 @@
-eventHub.addEventListener("click", (eventObject) => {
-    if (eventObject.target.id === "recordEntry") {
-        console.log(eventObject.target.id)
-        eventObject.preventDefault()
-        // Make a new object representation of a note
-        const newJournalEntry = {
-            <p>Date: ${entry.date}</p>,
-            <p>Entry: ${entry.entry}</p>,
-            <p>Mood: ${entry.mood}</p>
-        }
-        console.log(newJournalEntry)
+// eventHub.addEventListener("click", (eventObject) => {
+//     if (eventObject.target.id === "recordEntry") {
+//         console.log(eventObject.target.id)
+//         eventObject.preventDefault()
+//         // Make a new object representation of a note
+//         const newJournalEntry = {
+//             <p>Date: ${entry.date}</p>,
+//             <p>Entry: ${entry.entry}</p>,
+//             <p>Mood: ${entry.mood}</p>
+//         }
+//         console.log(newJournalEntry)
 
-        // Change API state and application state
-        saveNote(newNote)
-        .then(NoteList) // Refresh your list of notes once you've saved your new one
-    } 
-})
+//         // Change API state and application state
+//         saveNote(newNote)
+//         .then(NoteList) // Refresh your list of notes once you've saved your new one
+//     } 
+// })
